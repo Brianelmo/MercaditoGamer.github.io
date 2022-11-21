a743776535f282dc60317c7fddedfcec8dd9543a
Brian Elmo <brianelmo14@gmail.com>
2022-11-21 18:13:36 -0300

    Pantallas realizadas hasta el dia de hoy

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+// const lupa = document.querySelector('.lupa'); 
+// const barra = document.querySelector('.barra__busqueda');
+
+
+
+// lupa.addEventListener('mouseenter', () => {
+//   barra.style.display = 'block';
+//   lupa.style.position = 'absolute';  
+  
+//   setTimeout(() => {
+//     barra.style.display = 'none';
+//     lupa.style.position = 'relative';  
+//   }, 10000);
+
+// }) 
+

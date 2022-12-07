e657cac43c7583799c92b26c9bcd02d9cd398179
Brian Elmo <brianelmo14@gmail.com>
2022-12-07 18:56:22 -0300

    detalles

--- assets/controllers/controll_home.js
+++ assets/controllers/controll_home.js
@@ -58,5 +58,5 @@ const btnLogin = document.querySelector('#btn_login')
 
 
 btnLogin.addEventListener("click" , () => {
-  document.location.href = './assets/login.html'
+  document.location.href = 'assets/screens/login.html'
 })

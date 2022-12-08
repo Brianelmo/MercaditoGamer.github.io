0bce9a8ca19eaea0b609e90c038ee3a5ba3cc37e
Brian Elmo <brianelmo14@gmail.com>
2022-12-07 21:17:24 -0300

    mas cambios

--- assets/servicios/products.js
+++ assets/servicios/products.js
@@ -5,7 +5,7 @@ const server = 'https://data-base-ewra.onrender.com';
 const listProducts  = () => fetch(server + '/producto').then(respuesta => respuesta.json());  
 
 const crearCliente = (name , price , section , urlImage , id) => {
-  return fetch(server + `/producto${id}`, {
+  return fetch(server + `/producto/${id}`, {
     method: "POST", 
     headers: {
       "Content-Type" : "application/json"
@@ -21,20 +21,20 @@ const crearCliente = (name , price , section , urlImage , id) => {
 }  
 
 const deleteClient = (id) => {
-  return fetch(server + `/producto${id}`, {
+  return fetch(server + `/producto/${id}`, {
     method: "DELETE", 
   })
 } 
 
 
 const datosProduct = (id) => {
-  return fetch(server + `/producto${id}`).
+  return fetch(server + `/producto/${id}`).
   then(reponse  =>  reponse.json());
 }
  
 
 const editClient = (name , price , urlImage , section, id) => {
-  return fetch(server + `/producto${id}`, {
+  return fetch(server + `/producto/${id}`, {
     method:"PUT", 
     headers: {
       "Content-Type": "application/json"
